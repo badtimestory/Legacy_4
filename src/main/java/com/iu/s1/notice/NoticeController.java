beb5eefd532adcaa62db2a322df5989e0df4176e
simDH <jsy670711@gmail.com>
2022-02-18 19:34:38 +0900

    complete detail page

--- src/main/java/com/iu/s1/notice/NoticeController.java
+++ src/main/java/com/iu/s1/notice/NoticeController.java
@@ -1,9 +1,13 @@
 package com.iu.s1.notice;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("notice/*")
@@ -14,7 +18,19 @@ public class NoticeController {
 	
 	// list
 	@RequestMapping(value = "list", method = RequestMethod.GET)
-	public void list() throws Exception{
-		System.out.println("list 들어옴");
+	public ModelAndView list(ModelAndView mv) throws Exception {
+		List<NoticeDTO> ar = noticeService.list();
+		mv.addObject("list", ar);
+		mv.setViewName("notice/list");
+		
+		return mv;
 	}
+		
+	// detail
+	@RequestMapping(value = "detail", method = RequestMethod.GET)
+	public void detail(NoticeDTO noticeDTO, Model model) throws Exception {
+		noticeDTO = noticeService.detail(noticeDTO);
+		model.addAttribute("dto", noticeDTO);
+	}
+	
 }
--- src/main/webapp/WEB-INF/views/notice/list.jsp
+++ src/main/webapp/WEB-INF/views/notice/list.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +9,26 @@
 </head>
 <body>
   <h1>List Test Page</h1>
+  <table>
+    <tr>
+      <th>NUM</th>
+      <th>TITLE</th>
+      <th>CONTENTS</th>
+      <th>WRITER</th>
+      <th>REGDATE</th>
+      <th>HIT</th>
+    </tr>
+    
+   <c:forEach items="${list}" var="notice">
+     <tr>
+       <td>${notice.num}</td>
+       <td><a href="./detail?num=${notice.num}">${notice.title}</a></td>
+       <td>${notice.contents}</td>
+       <td>${notice.writer}</td>
+       <td>${notice.regDate}</td>
+       <td>${notice.hit}</td>
+    </tr>
+  </c:forEach>
+  </table>
 </body>
 </html>
